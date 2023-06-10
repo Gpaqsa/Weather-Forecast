26937b3b149a0152e24325acf266e39c2f47a537
Gpaqsa <giorgi.paksashvili.2@btu.edu.ge>
2023-06-10 14:13:42 +0400

    add Data from api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-
-import './App.css';
-
+import React, { useState } from "react";
+import axios from "axios";
 function App() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=dalas&appid=6a03361273178414adeb1036973a5d2e`;
+
   return (
-    <div className="App">
-     
+    <div className="app">
+      <div className="container">
+        <section className="top">
+          <article className="location">
+            <p>Dalas</p>
+          </article>
+          <article className="temp">
+            <h1>65°C</h1>
+          </article>
+          <article className="description">
+            <p>Clouds</p>
+          </article>
+        </section>
+        <section className="Buttom">
+          <article className="feels">
+            <p>65°C</p>
+          </article>
+          <article className="humidity">
+            <p>20%</p>
+          </article>
+          <article className="wind">12 MPH</article>
+        </section>
+      </div>
     </div>
   );
 }
